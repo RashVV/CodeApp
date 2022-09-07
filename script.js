bf8d97eeba7d248d09e41f7b4beb997a3e6c2143
RashVV <vadim.ratushnyak@gmail.com>
2022-09-07 19:31:19 +0300

    add function for colecting data from input

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const post = createPost('New period of my life', 'Today I have one of the best d
 //const post = createPost('New period of my life' )
 
 console.log('Post:', post)
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const name = form.name.value
+    const role = form.role.value
+    const email = form.email.value
+
+    saveLid ({name, role, email})
+})
+
+function saveLid(data) {
+    const formData = {
+        date : new Date().toLocaleDateString(),
+        ...data
+        //name: data.name,
+        //role: data.role,
+        //email: data.email
+    }
+console.log('Lid', formData)
+}
