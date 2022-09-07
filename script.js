2daa0490436011e887d6be12e3c3a81efffaa783
RashVV <vadim.ratushnyak@gmail.com>
2022-09-07 21:33:03 +0300

    add dinamic by js

--- script.js
+++ script.js
@@ -51,5 +51,67 @@ menu.insertAdjacentHTML('afterbegin', amp)
 menu.insertAdjacentHTML('afterbegin', game)
 
 
-//
+//Class RootElement <= Box <= Instance
 
+
+class RootElement {
+    constructor(tagName = 'div') {
+        this.$el = document.createElement(tagName)
+        this.$el.style.marginBottom = '13px'
+    }
+    hide() {
+        this.$el.style.opacity = '0'
+    }
+    show() {
+        this.$el.style.opacity = '1'
+    }
+    apend() {
+        document.querySelector('.app').insertAdjacentElement('beforeend', this.$el)
+    }
+}
+
+class Box extends RootElement {
+    constructor(color, size = 150, tagName) {
+        super(tagName)
+        this.color = color
+        this.size = size
+    }
+    create() {
+        //console.log(this.$el)
+        this.$el.style.backgroundColor = this.color
+        this.$el.style.width = this.$el.style.height = `${this.size}px`
+
+        this.apend()
+        return this
+    }
+}
+
+class Circle extends RootElement {
+    constructor(color) {
+        super();
+        this.color = color
+    }
+    create() {
+        this.$el.style.borderRadius = '50%'
+        this.$el.style.width = this.$el.style.height = '120px'
+        this.$el.style.background = this.color
+
+        this.apend()
+
+        return this
+
+    }
+}
+
+const redBox = new Box('red', 100, 'div').create()
+const blueBox = new Box('blue', 100, 'div').create()
+
+const circle = new Circle('green').create()
+
+circle.$el.addEventListener('mouseenter', () => {
+    circle.hide()
+})
+
+circle.$el.addEventListener('mouseleave', () => {
+    circle.show()
+})
